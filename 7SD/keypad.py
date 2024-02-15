549e7ac068a7ad95b8208fa98b189b185f5579a8
Saketh Medicherla <saketh.medicherla@gmail.com>
2024-02-15 15:29:04 -0500

    Keypad working
    Need to work on # key toggle functionality

--- 7SD/keypad.py
+++ 7SD/keypad.py
@@ -11,19 +11,19 @@ Y2 = 16
 Y3 = 20
 Y4 = 21
 
-o1 = 4
-o2 = 17
-o3 = 27
-o4 = 22
-o5 = 5
-o6 = 6
-o7 = 13
-o8 = 19
+e = 4
+d = 17
+c = 27
+dot = 22
+b = 5
+a = 6
+f = 13
+g = 19
 
 clk = 18
 #ctrl = 26
-dff_pins = [o8,o7,o6,o5,o4,o3,o2,o1]
-GPIO.setup([X1,X2,X3,X4], GPIO.OUT)
+dff_pins = [a,b,c,d,e,f,g]
+GPIO.setup([X1,X2,X3,X4, dot], GPIO.OUT)
 GPIO.setup([Y1,Y2,Y3,Y4], GPIO.IN)
 GPIO.setup(dff_pins, GPIO.OUT)
 GPIO.setup(clk, GPIO.OUT)
@@ -38,39 +38,52 @@ def readKeypad(rowNum,char):
 	GPIO.output(rowNum,GPIO.LOW)
 	return curVal
 
-bin_vals = {0:[1,1,1,1,1,1,0,0], 
-			1:[0,0,0,1,0,1,0,0], 
-			2:[0,1,0,1,0,0,1,0], 
-			3:[1,0,1,1,0,1,1,0], 
-			4:[1,1,0,1,1,1,0,0], 
-			5:[1,0,1,1,0,1,1,0], 
-			6:[1,0,1,1,1,1,1,0], 
-			7:[1,1,1,0,0,0,0,0], 
-			8:[1,1,1,1,1,1,1,0], 
-			9:[1,1,1,1,0,1,1,0]
+bin_vals = {0:[1,1,1,1,1,1,0], 
+			1:[0,1,1,0,0,0,0], 
+			2:[1,1,0,1,1,0,1], 
+			3:[1,1,1,1,0,0,1], 
+			4:[0,1,1,0,0,1,1], 
+			5:[1,0,1,1,0,1,1], 
+			6:[1,0,1,1,1,1,1], 
+			7:[1,1,1,0,0,0,0], 
+			8:[1,1,1,1,1,1,1], 
+			9:[1,1,1,1,0,1,1],            
+            'A':[1,1,1,0,1,1,1], 
+            'B':[0,0,1,1,1,1,1], 
+            'C':[0,0,0,1,1,0,1], 
+            'D':[0,1,1,1,1,0,1], 
             }
-'''
-            'A':[1,1,1,1,1,1,1,1],
-            'B':[0,0,0,0,0,0,1,1],
-            'C':[0,1,0,1,0,1,0,1],
-            'D':[0,1,1,1,0,1,0,1]
-            }
-'''
+
 def output(gpio_lst, states):
 	for i in range(len(gpio_lst)):
 		GPIO.output(gpio_lst[i], states[i])
+		
+def switch(gpio_lst):
+    last_states = [GPIO.input(i) for i in dff_pins]
+    if 1 in last_states: output(gpio_lst, [0,0,0,0,0,0,0])    
+    else: output(gpio_lst, last_states)
 
 def ssd_disp(clk_num, value):
-	try:
-		value = int(value)
-		output(dff_pins, bin_vals[value])
-	except:
-		pass
+    try:
+        value = int(value)
+        output(dff_pins, bin_vals[value])
+    except:
+        if value == 'A': output(dff_pins, bin_vals['A'])
+        if value == 'B': output(dff_pins, bin_vals['B'])
+        if value == 'C': output(dff_pins, bin_vals['C'])
+        if value == 'D': output(dff_pins, bin_vals['D'])
+        if value == '*':
+            if GPIO.input(dot) == 0: GPIO.output(dot, 1)
+            else: GPIO.output(dot, 0)
+        if value == '#':
+            switch(dff_pins)
+            
+        pass
 
 
 def latch_value():
 	GPIO.output(clk, 1)
-	sleep(0.01)
+	sleep(0.05)
 	GPIO.output(clk, 0)
 
 try:
@@ -84,8 +97,11 @@ try:
 		ssd_disp(clk, readKeypad(X1, [1,2,3,'A']))
 		ssd_disp(clk, readKeypad(X2, [4,5,6,'B']))
 		ssd_disp(clk, readKeypad(X3, [7,8,9,'C']))
+		ssd_disp(clk, readKeypad(X4, ['*',0,'#','D']))
 		latch_value()
-		sleep(1)
+		sleep(.2)
+		
+            
 		# for i in range(len(dff_pins)):
 		# 	GPIO.output(dff_pins[i], bin_vals[1][i])
 except KeyboardInterrupt: 
