0f41b7f8e21800ce7d392a61250a89798387dd65
Saketh Medicherla <saketh.medicherla@gmail.com>
2024-02-16 06:30:55 -0500

    Uploaded assembly code and screenshots

---
+++ EquReqData/6/6_strb_ex2_3.s
@@ -0,0 +1,35 @@
+	@ Deliverable1: State the contents of memory locations at data_store after the program is executed.  Add instructions prior to the 
+ 	@ the last two instructions to show the contents of data_store in registers R2 and R3
+	@ Deliverable2: What does the strb instruction do?
+ 	@ Deliverable3: What does the .space compiler directive do?
+
+	.global _start
+_start:
+	mov	r1, #0x99	@ r1 = 0x99
+	ldr	r6, =data_store	@ r6 = data_store pointer
+	strb	r1, [r6]	@ store r1 into location pointed to by r6 
+				@ 
+	add	r6, r6, #1	@ r6 = r6 + 1
+	mov	r1, #0x85	@ r1 = 0x85
+	strb	r1, [r6]	@ store r1 into location pointed to by r6 
+				@ 
+	add	r6, r6, #1	@ r6 = r6 + 1
+	mov	r1, #0x3f	@ r1 = 0x3f
+	strb	r1, [r6]	@ store r1 into location pointed to by r6 
+
+	add	r6, r6, #1	@ r6 = r6 + 1
+	mov	r1, #0x63	@ r1 = 0x63
+	strb	r1, [r6]	@ store r1 into location pointed to by r6 
+
+	add	r6, r6, #1	@ r6 = r6 + 1
+	mov	r1, #0x12	@ r1 = 0x12
+	strb	r1, [r6]	@stores the byte r1 to location r6 
+
+	ldr r2, =data_store @added lines
+	ldr r3, [r2]    
+	
+	mov	r7, #1
+	svc	0
+ 
+	.data
+ data_store: .space 8
