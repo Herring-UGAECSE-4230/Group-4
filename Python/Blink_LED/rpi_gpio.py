ed49e75e61f36a8e42d260aef1472749c54380eb
jco34662 <jco34662@uga.edu>
2024-03-17 19:07:10 -0400

    finished MC encoder

--- Python/Blink_LED/rpi_gpio.py
+++ Python/Blink_LED/rpi_gpio.py
@@ -3,19 +3,19 @@ from time import sleep
 
 # GPIO set up for ignoring console warnings and using physical pin numbers
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 # Set up pin 7 as an output pin
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
 
 # In the while loop, the output is set manually to low or high in certain time intervals
 try:
     while True:
         # Changing time interval changes frequency
-        GPIO.output(7, GPIO.HIGH)
-        sleep(.5)
-        GPIO.output(7, GPIO.LOW)
-        sleep(.5)
+        GPIO.output(5, GPIO.HIGH)
+        sleep(.2)
+        GPIO.output(5, GPIO.LOW)
+        sleep(.2)
 
 # Reset all pins when program is ended
 except KeyboardInterrupt:
--- Python/Morse Code/mctest.py
+++ Python/Morse Code/mctest.py
@@ -1,35 +1,38 @@
 from time import sleep
+from time import perf_counter
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-pin = 16
-GPIO.setup(pin, GPIO.OUT)
+LED = 5
+speaker = 6
+unit_length = float(input('Enter the desired unit length in seconds: '))
+
+GPIO.setup([LED, speaker], GPIO.OUT)
 
 def beep(delay):
-    GPIO.output(pin, 1)
+    GPIO.output([LED, speaker], 1)
     sleep(delay)
-    GPIO.output(pin, 0)
+    GPIO.output([LED, speaker], 0)
     sleep(0.5)
     
 def mc_inp(char):
     if char == '-':
-        beep(.5)
+        beep(2*unit_length)
     elif char == '.':
-        beep(0.25)
+        beep(unit_length)
     else:
         sleep(0.25)
         
-test = '..--'
-# 
-# for i in test:
-#     mc_inp(i)
-#
+
 
 with open('output.txt') as file:
     lines = [line.rstrip().lstrip().split('|')[0] for line in file.readlines()]
     
-for line in lines:
-    print(line)
     
 for line in lines:
+    print(line)
     for ch in line:
+        
+#         time1 = perf_counter()
         mc_inp(ch)
+#         time2 = perf_counter()
+#         print("Time elapsed: ", time2 - time1)
